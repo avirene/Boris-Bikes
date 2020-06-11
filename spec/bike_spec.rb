2043649ec67f718897eb35af87b4443fc240b2ed
Iryna Audzei <avirene@live.com>
2020-06-11 15:33:13 +0000

    Added rising an exeption (step12)

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,6 @@
 require "./lib/bike"
 describe Bike do
- it { is_expected.to respond_to :working? }
+ # it { is_expected.to respond_to :working? }
  
  it "checks bike is working" do 
     expect(subject.working?).to eq true
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,25 @@
 require './lib/docking_station'
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
-  
-  it 'is an instance of Bike class' do
-    expect(subject.release_bike).to be_instance_of(Bike)
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      # we want to release the bike we docked
+      expect(subject.release_bike).to eq bike
+    end
+  end
+  # it { is_expected.to respond_to :release_bike }
+  describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
  
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
+    bike.working?
     expect(bike).to be_working
   end
 
@@ -22,7 +34,6 @@ describe DockingStation do
   it 'returns docked bikes' do
     bike = Bike.new
     subject.dock(bike)
-    # Again, we need to return the bike we just docked
     expect(subject.bike).to eq bike
   end
   
